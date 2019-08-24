a5c27651aba29171039223e41c5ceddbd9f76480
Jonatan Larsson <jonatanlarsson2001@gmail.com>
2019-08-25 01:02:32 +1100

    Edited the print function of the Printer class.

---
+++ src/mainpackage/ColorStorer.java
@@ -0,0 +1,17 @@
+package mainpackage;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author jonat
+ */
+public class ColorStorer {
+
+    final Color COLOR;
+
+    ColorStorer(Color color) {
+        COLOR = color;
+    }
+
+}
